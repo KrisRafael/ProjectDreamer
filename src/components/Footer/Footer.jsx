65605e10e033c176386448f4416a7f30ca46df82
KrisRafael <Krhovjakova.K@email.cz>
2024-11-26 00:48:07 +0100

    Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import { IconBrandFacebook, IconBrandInstagram, IconBrandX } from "@tabler/icons-react";
+
 import classes from "./Footer.module.css";
 
 export function Footer() {
@@ -5,6 +7,17 @@ export function Footer() {
       <div className={classes.footerContainer}>
           <footer className={classes.footer}>
             <p style={{fontWeight: "bold", fontSize: "13px",    color: "dodgerblue", marginLeft: "25px"}}>© 2024      Copyright by Kris</p>
+            <div className={classes.icons}>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className={classes.iconLink}>
+                        <IconBrandFacebook className={classes.icon} />
+                    </a>
+                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className={classes.iconLink}>
+                        <IconBrandInstagram className={classes.icon} />
+                    </a>
+                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className={classes.iconLink}>
+                        <IconBrandX className={classes.icon} />
+                    </a>
+            </div>
           </footer>
       </div>  
    )
